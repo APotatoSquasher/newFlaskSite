f6810dad48eba33ed61b00cbda8a28518935bce0
Yousef Nassr <yousefnassr09@gmail.com>
2024-12-23 11:51:49 -0600

    added css and photos

--- whynot.py
+++ whynot.py
@@ -1,12 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 import os
 # Creates a Flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "test case i know this is public"
+# Creates a Form class
+class NamerForm(FlaskForm):
+    # StringField is the box where you type in things. If you didn't fill out the form,
+    # validator will pop up and say you didn't fill out the form, datarequired just checks if you put anything in
+    # submit is a button that sends the info
+    name=StringField("What's Your name?",validators=[DataRequired()])
+    submit=SubmitField("Submit")
+    #there are many other fields, like BooleanField, DateTimeField, PasswordField, etc.
 # Creates a route decorator
 if __name__ == "__main__":
     app.run(debug=False) # this runs
 @app.route('/')
-def index():
+def index(): #Accessed from the home button and is by default, look in navbar.html (done via {{ url_for('')) }}
     stuff="Chat this is NOT a good idea!"
     toppings=["Cheese","Olives","Pepper",3,15]
     return render_template("index2.html",stuff=stuff,pizza_toppings=toppings)
@@ -21,10 +33,21 @@ def info():
 #striptags removes tags. trim removes the last spaces in a sentence.
 #You can also access a list item by using. for example: pizzatoppings.0
 
-#Create invalid URL page
+#Create invalid URL page, it MUST be in this format
 @app.errorhandler(404)
 def PageNotFound(error):
     return render_template('404.html'), 404
-#@app.errorhandler(500)
-#def Bug():
-#    return "<h1> It appears you have found a bug in my code! Feel free to email me! (Or this could be me and I'm telling myself I'm an idiot...)<h1/>"
+#Creates page for server error
+@app.errorhandler(500)
+def Bug(error):
+    return render_template('500.html'), 500
+@app.route('/signin',methods=['GET','POST']) #POST is when you submit something
+def SignIn():
+    name=None #name is None unless the user submits something
+    form = NamerForm() #References the class above NamerForm()
+    if form.validate_on_submit(): #Validates form by taking what ever name is on the text box
+        # and resetting the box
+        name=form.name.data
+        form.name.data=''
+        flash('Form Submitted Successfully')# flashes a message from the imported flash library
+    return render_template('name.html',name=name,form=form) #passes variables to name.html
