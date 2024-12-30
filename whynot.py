72ad6769686338ff84e22b301c04bc9973fe89e9
Yousef Nassr <yousefnassr09@gmail.com>
2024-12-29 20:50:02 -0600

    Added users system

--- whynot.py
+++ whynot.py
@@ -1,12 +1,52 @@
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 import os
 # Creates a Flask Instance
 app = Flask(__name__)
+#Add database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password123!@localhost/our_users'
+
 app.config['SECRET_KEY'] = "test case i know this is public"
+# initalize database
+db = SQLAlchemy(app)
+
+
+# Create Model
+class Users(db.Model):
+    # creates id, name, email, and date for each person. Primary key automatically makes the id
+    id= db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(200),nullable=False) #200 is the max amount of string. Cannot be nothing as per nullable=False
+    email=db.Column(db.String(120),nullable=False,unique=True) #only one email per user allowed due to unique=
+    date_added=db.Column(db.DateTime, default=datetime.now) # puts current date when they fill out the form
+    # Creates a String
+    def __repr__(self):
+        return '<Name %r>' % self.name
 # Creates a Form class
+class UserForm(FlaskForm):
+    name=StringField("Name: ",validators=[DataRequired()])
+    email=StringField("Email: ",validators=[DataRequired()])
+    submit=SubmitField("Submit")
+# Update database
+@app.route('/update/<int:id>',methods=['GET','POST'])
+def update(id): # Is <int:id> in this case
+    form = UserForm()
+    name_to_update = Users.query.get_or_404(id)
+    if request.method == "POST":
+        name_to_update.name = request.form['name'] # Getting name and email of the previously submitted form
+        name_to_update.email = request.form['email']
+        try:
+            db.session.commit() # Database can submit name_to_update because it accesses Users, the database
+            flash("User Updated Successfully")
+            return render_template("update.html",form=form,name_to_update=name_to_update)
+        except:
+            flash("Error!")
+    else:
+        return render_template("update.html",form=form,name_to_update=name_to_update)
 class NamerForm(FlaskForm):
     # StringField is the box where you type in things. If you didn't fill out the form,
     # validator will pop up and say you didn't fill out the form, datarequired just checks if you put anything in
@@ -17,9 +57,25 @@ class NamerForm(FlaskForm):
 # Creates a route decorator
 if __name__ == "__main__":
     app.run(debug=False) # this runs
+@app.route('/user/add',methods=['GET','POST'])
+def add_user():
+    name=None
+    form = UserForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first() # Grabs all emails with what was typed in and get the first one. There shouldn't be any, it should return None
+        if user is None:
+            user= Users(name=form.name.data,email=form.email.data) # sets user to the name and email given
+            db.session.add(user) # adds user to the database
+            db.session.commit() # commits it
+        name = form.name.data 
+        form.name.data = "" # Resets the name and email back
+        form.email.data = ""
+        flash("Welcome to my blog!")
+    our_users= Users.query.order_by(Users.date_added) # Gets a list of users by date added
+    return render_template("add_user.html",form=form,name=name,our_users=our_users)
 @app.route('/')
 def index(): #Accessed from the home button and is by default, look in navbar.html (done via {{ url_for('')) }}
-    stuff="Chat this is NOT a good idea!"
+    stuff="Homepage"
     toppings=["Cheese","Olives","Pepper",3,15]
     return render_template("index2.html",stuff=stuff,pizza_toppings=toppings)
 # localhost:5000/user/John
